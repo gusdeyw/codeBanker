73d310cb21313d443152440d85faab2090655b8f
Gusde Widnyana <widnyana.bagus@gmail.com>
2022-10-10 15:55:01 +0800

    Create Pagination PHP
    Not Completed Yet

---
+++ PHP/paginate.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <title>Test Document</title>
+</head>
+<body>
+
+<?php
+$total_page = 20;
+
+
+
+?>
+
+<select name="" id="page-select">
+    <?php for($i=1; $i <= $total_page; $i++){
+        
+    } 
+    ?>
+    <option value=""></option>
+</select>
+    
+</body>
+</html>
+
